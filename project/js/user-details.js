695fc7315190188c41837dac393c17761a8e0c49
stark2288915 <stark2288915@gamil.com>
2024-01-12 14:13:28 +0200

    dd

--- project/js/user-details.js
+++ project/js/user-details.js
@@ -1,1 +1,29 @@
-console.log('hello');
+const localUrl = new URL(location.href);
+const user =  JSON.parse(localUrl.searchParams.get('user'));
+
+const ul = document.getElementById('infoList');
+
+console.log(user);
+
+function showUser (user) {
+
+    for (const key in user) {
+
+        if(typeof user[key] === 'object'){
+            const h3 = document.createElement('h3');
+            h3.innerText = key;
+            ul.appendChild(h3);
+
+            showUser(user[key]);
+
+        }else{
+            const li = document.createElement('li');
+            li.innerText = `${key}: ${user[key]}`;
+            ul.appendChild(li);
+        }
+
+
+    }
+}
+
+showUser(user);
