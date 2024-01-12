72c44dc39c56923c4fbfef05fa59aa69815989d1
stark2288915 <stark2288915@gamil.com>
2024-01-12 13:40:00 +0200

    dd

--- project/js/index.js
+++ project/js/index.js
@@ -1,5 +1,25 @@
-console.log("fsdh");
+
+const allUsersDiv = document.getElementById('allUsers');
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(users => users.json())
-    .then(users => console.log(users));
+    .then(users => {
+        users.forEach(user => {
+            const userDiv = document.createElement('div');
+            userDiv.classList.add('userDiv');
+
+            const idNameUser = document.createElement('p');
+            idNameUser.classList.add('idNameUser');
+            idNameUser.innerText = `id:${user.id}  name:${user.name}`;
+
+            const buttonShowUser = document.createElement('button');
+            buttonShowUser.innerText = 'show details';
+            buttonShowUser.addEventListener('click', () => {
+                location.href = `user-details.html?user=${JSON.stringify(user)}`;
+            })
+
+            allUsersDiv.appendChild(userDiv);
+            userDiv.appendChild(idNameUser);
+            userDiv.appendChild(buttonShowUser);
+        })
+    });
